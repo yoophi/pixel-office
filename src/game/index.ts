37630db0f738537f07a3a660a23ee769fe583aa4
Pyunghyuk Yoo <yoophi@robin.local>
2026-04-21 23:58:17 +0900

    Add Tiled map loader helpers

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,1 +1,19 @@
-export {};
+export { PhaserGame } from './PhaserGame.js';
+export {
+  createOfficeFromTiledMap,
+  createPhaserTilemap,
+  preloadTiledMapAssets,
+} from './tiled/loader.js';
+export type {
+  LoadedPhaserTilemap,
+  TiledMap,
+  TiledObject,
+  TiledObjectLayer,
+  TiledTileLayer,
+  TiledTileset,
+} from './tiled/loader.js';
+export {
+  DEFAULT_TILESET_VARIANTS,
+  findTilesetVariant,
+  getTilesetImageKey,
+} from './tiled/tilesetVariants.js';
